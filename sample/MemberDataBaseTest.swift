8a7a4e9a25f289d6ff09b7bfb27050ad70631a92
Atsuki Demizu <demmys.atsuki@gmail.com>
2015-06-17 14:17:58 +0900

    Change TestUnit to class from protocol to create TestUnit more easily

--- sample/MemberDataBaseTest.swift
+++ sample/MemberDataBaseTest.swift
@@ -1,43 +1,32 @@
 import PureSwiftUnit
 
 class MemberDataBaseTest : TestUnit {
-    var description: String { get { return "MemberDataBase class" } }
-
-    var testCases: [TestCase] { get { return [
-        TestCase(self,
-                 test: canAddMember,
-                 description: "can add member via addMember method"),
-        TestCase(self,
-                 test: canRemoveMember,
-                 description: "can remove member via removeMember method"),
-        TestCase(self,
-                 test: canClearAll,
-                 description: "can clear all member via clearAll method")
-    ] } }
+    init() {
+        super.init("MemberDataBase class")
+        setTestCases([
+            ("can add member via addMember method", canAddMember),
+            ("can remove member via removeMember method", canRemoveMember),
+            ("can clear all member via clearAll method", canClearAll)
+        ])
+    }
 
     var db: MemberDataBase!
 
-    func setUp() {}
-
-    func beforeCase() {
+    override func beforeCase() {
         db = MemberDataBase()
     }
-
-    func afterCase() {}
-
-    func tearDown() {}
 
     private func canAddMember() -> TestResult {
         let id = db.addMember("John Lennon")
         guard db.memberCount() == 1 else {
-            return TestResult.buildFailure("member count",
-                                           expected: 1,
-                                           actual: db.memberCount())
+            return TestResult.buildFailure(
+                "member count", expected: 1, actual: db.memberCount()
+            )
         }
         guard db.nextId() == id + 1 else {
-            return TestResult.buildFailure("next id",
-                                           expected: id + 1,
-                                           actual: db.nextId())
+            return TestResult.buildFailure(
+                "next id", expected: id + 1, actual: db.nextId()
+            )
         }
         return .Success
     }
@@ -46,19 +35,19 @@ class MemberDataBaseTest : TestUnit {
         let id = db.addMember("John Lennon")
         let result = db.removeMember(id)
         guard result else {
-            return TestResult.buildFailure("result of removeMember",
-                                           expected: "true",
-                                           actual: "false")
+            return TestResult.buildFailure(
+                "result of removeMember", expected: "true", actual: "false"
+            )
         }
         guard db.memberCount() == 0 else {
-            return TestResult.buildFailure("member count",
-                                           expected: 0,
-                                           actual: db.memberCount())
+            return TestResult.buildFailure(
+                "member count", expected: 0, actual: db.memberCount()
+            )
         }
         guard db.nextId() == id + 1 else {
-            return TestResult.buildFailure("next id",
-                                           expected: id + 1,
-                                           actual: db.nextId())
+            return TestResult.buildFailure(
+                "next id", expected: id + 1, actual: db.nextId()
+            )
         }
         return .Success
     }
--- src/TestUnit.swift
+++ src/TestUnit.swift
@@ -1,10 +1,22 @@
-public protocol TestUnit {
-    var description: String { get }
-    var testCases: [TestCase] { get }
-    func setUp()
-    func beforeCase()
-    func afterCase()
-    func tearDown()
+public class TestUnit {
+    let description: String
+    var testCases: [TestCase] = []
+
+    public init(_ description: String) {
+        self.description = description
+    }
+
+    public func setUp() {}
+    public func tearDown() {}
+    public func beforeCase() {}
+    public func afterCase() {}
+
+    public func setTestCases(tests: [(String, () -> TestResult)]) {
+        self.testCases = []
+        for (d, t) in tests {
+            self.testCases.append(TestCase(manager: self, test: t, description: d))
+        }
+    }
 }
 
 public enum TestResult {
@@ -12,17 +24,19 @@ public enum TestResult {
     case Failure(String)
     case Pending(String)
 
-    public static func buildFailure(parameter: String,
-                                    expected: String,
-                                    actual: String) -> TestResult {
-        return .Failure("expected \(parameter) to \(expected) but actual is \(actual).")
+    public static func buildFailure(
+        parameter: String, expected: String, actual: String
+    ) -> TestResult {
+        return .Failure(
+            "expected \(parameter) to \(expected) but actual is \(actual)."
+        )
     }
-    public static func buildFailure(parameter: String,
-                                    expected: Int,
-                                    actual: Int) -> TestResult {
-        return TestResult.buildFailure(parameter,
-                                       expected: String(expected),
-                                       actual: String(actual))
+    public static func buildFailure(
+        parameter: String, expected: Int, actual: Int
+    ) -> TestResult {
+        return TestResult.buildFailure(
+            parameter, expected: String(expected), actual: String(actual)
+        )
     }
 }
 
@@ -31,7 +45,7 @@ public class TestCase {
     private let test: () -> TestResult
     public let description: String
 
-    public init(_ manager: TestUnit, test: () -> TestResult, description: String) {
+    public init(manager: TestUnit, test: () -> TestResult, description: String) {
         self.manager = manager
         self.test = test
         self.description = description
